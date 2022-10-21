6ed91734771880fc17ce3e39f31ece2e4efb9fd3
Marcelo Maia Lima <marcelomaiajp@gmail.com>
2022-10-21 16:33:30 -0300

    template aula

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import styled, { createGlobalStyle } from "styled-components";
+import TelaLogin from "./components/TelaLogin/TelaLogin";
+import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+
+const GlobalStyled = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
+const MainContainer = styled.main`
+  height: 100vh;
+`
+
+function App() {
+
+  return (
+    <MainContainer >
+      <GlobalStyled />
+      <TelaLogin />
+      <TelaCadastro />
+    </MainContainer>
+  );
+}
+
+export default App;
